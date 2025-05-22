35900bb010f2cb1dc805b2442f3362c53e144f38
Jonas Dech <j.dech@gmx.net>
2025-05-22 10:12:25 +0200

    changed to ROS2 package

--- setup.py
+++ setup.py
@@ -1,12 +1,36 @@
-#!/usr/bin/env python3
+import os
+from glob import glob
+from setuptools import setup
 
-from os.path import dirname, abspath, basename
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+package_name = 'knowrob_designator'
 
-setup_args = generate_distutils_setup(
-    packages=[basename(dirname(abspath(__file__)))],
-    package_dir={'': 'src'},
+setup(
+    name=package_name,
+    version='0.0.0',
+    # Packages to export
+    packages=[package_name],
+    # Files we want to install, specifically launch files
+    data_files=[
+        # Install marker file in the package index
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        # Include our package.xml file
+        (os.path.join('share', package_name), ['package.xml']),
+        # Include all launch files.
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py'))),
+    ],
+    # This is important as well
+    install_requires=['setuptools'],
+    zip_safe=True,
+    author='Sascha Jongebloed',
+    author_email='ros2@ros.com',
+    maintainer='Sascha Jongebloed',
+    description='Knowrob designator package',
+    license='TODO',
+    # Like the CMakeLists add_executable macro, you can add your python
+    # scripts here.
+    entry_points={
+        'console_scripts': [
+            'my_script = my_package.my_script:main'
+        ],
+    },
 )
-
-setup(**setup_args)
